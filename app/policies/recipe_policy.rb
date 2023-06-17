5fedae15d28be4e1147ed4568754987ff9f2ed18
Lena Sachse <lena.sachse@t-online.de>
2023-06-17 11:41:11 +0200

    added controller and pundit

--- app/policies/recipe_policy.rb
+++ app/policies/recipe_policy.rb
@@ -1,8 +1,17 @@
 class RecipePolicy < ApplicationPolicy
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    class RecipePolicy < ApplicationPolicy
+      def show?
+        true
+      end
+
+      def create?
+        user.present?
+      end
+
+      def new?
+        create?
+      end
+    end
   end
 end
