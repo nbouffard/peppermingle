abd60f4e79aacf9fb6b36566cc3d984a7413f6cf
Lena Sachse <lena.sachse@t-online.de>
2023-06-17 17:03:03 +0200

    started working on the show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_000446) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_063123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,9 +80,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_18_000446) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
     t.string "first_name"
     t.string "last_name"
+    t.string "nickname"
     t.string "location"
     t.date "date_of_birth"
     t.text "bio"
