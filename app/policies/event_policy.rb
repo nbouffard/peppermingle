22ab4322006deb5261e9fc770baa8f94277c12db
igor dinic <dinicigor84@gmail.com>
2023-06-19 20:37:16 +0200

    policy change

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -5,4 +5,24 @@ class EventPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def show?
+    true
+  end
 end
