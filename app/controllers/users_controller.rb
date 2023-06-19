3eca07c791a269a8f84b0b7e9a8c63c855038f04
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-19 20:27:45 +0200

    add user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize @user
   end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.create(user_params)
+      redirect_to user_path(@user)
+    else
+      render :show, status: :unproccessable_entity
+    end
+  end
 end
