b0c6bd0b0ea9e7e595fb09825b4d34d99facb7ff
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-20 09:29:19 +0200

    add set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,25 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
+  before_action :set_user
+
   def show
-    @user = User.find(params[:id])
-    authorize @user
+  end
+
+  def edit
   end
 
   def update
-    @user = User.find(params[:id])
-    authorize @user
     if @user.create(user_params)
       redirect_to user_path(@user)
     else
       render :show, status: :unproccessable_entity
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
   end
 end
