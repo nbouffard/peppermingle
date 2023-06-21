fe8e13e6df03d9f344e1e4ac7b924f227e5c80c1
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-21 13:49:16 +0200

    removed select_tags

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,13 +7,13 @@ class Recipe < ApplicationRecord
   DIET = ["Vegan", "Dairy free", "Gluten free", "Nut free", "Vegetarian", "pescatarian"]
   validates :title, :description, :ingredients, :cuisine, :difficulty, :directions, presence: true
   validates :prep_time, :total_time, :servings, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :season, inclusion: { in: SEASONS }
-  validates :meal_type, inclusion: { in: MEAL_TYPES }
-  validates :dietary_requirements, inclusion: { in: DIET }
+  # validates :season, inclusion: { in: SEASONS }
+  # validates :meal_type, inclusion: { in: MEAL_TYPES }
+  # validates :dietary_requirements, inclusion: { in: DIET }
 
   include PgSearch::Model
   pg_search_scope :filter_recipes,
-                  against: %i[meal_type season dietary_requirements],
+                  against: %i[meal_type season dietary_requirements title cuisine difficulty total_time],
                   using: {
                     tsearch: { prefix: true }
                   }
