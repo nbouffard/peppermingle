dc85fd14baf6ef92e9c4fcf585c81042c354f733
Quan <99978041+ShimaGunz@users.noreply.github.com>
2023-06-22 15:53:48 +0100

    move delete button

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,4 @@ class Recipe < ApplicationRecord
 
   validates :title, :description, :ingredients, :category, :cuisine, :difficulty, :directions, presence: true
   validates :prep_time, :total_time, :servings, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
 end
