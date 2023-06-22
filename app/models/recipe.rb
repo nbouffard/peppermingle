606a505ab2348473df1f1c4f790e48c72afde3f9
Lena Sachse <lena.sachse@t-online.de>
2023-06-22 18:25:22 +0200

    updated the show frontend

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :events
+  has_many :ingredients, dependent: :destroy
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
 
   validates :title, :description, :ingredients, :category, :cuisine, :difficulty, :directions, presence: true
   validates :prep_time, :total_time, :servings, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
