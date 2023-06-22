7f67c300ed81edcdbc526a31967993b4007906a2
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-22 21:26:10 +0200

    add targets

--- app/javascript/controllers/filter_recipes_controller.js
+++ app/javascript/controllers/filter_recipes_controller.js
@@ -4,9 +4,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "recipes", "input"];
   connect() {
-    console.log('hello')
-    console.log(this.formTarget);
-    console.log(this.recipesTarget);
+    // console.log('hello')
+    // console.log(this.formTarget);
+    // console.log(this.recipesTarget);
   }
 
   update() {
