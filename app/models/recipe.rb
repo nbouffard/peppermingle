963efe339557f3d65913deee35b7f81c5a238d4d
Lena Sachse <lena.sachse@t-online.de>
2023-06-23 18:35:42 +0200

    added cloudinary

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :ingredients, dependent: :destroy
   accepts_nested_attributes_for :ingredients, allow_destroy: true
+  has_many_attached :images
 
 
   validates :title, :description, :category, :cuisine, :difficulty, :directions, :prep_time, :total_time, :servings, presence: true
