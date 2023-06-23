a57b6c1e518ae7ca44f413177377e0b5dccc28e7
Lena Sachse <lena.sachse@t-online.de>
2023-06-23 20:21:38 +0200

    added partial files for an image carousel and the add ingredients part

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,6 @@ class Recipe < ApplicationRecord
   has_many_attached :images
 
 
-  validates :title, :description, :category, :cuisine, :difficulty, :directions, :prep_time, :total_time, :servings, presence: true
+  validates :title, :description, :cuisine, :difficulty, :directions, :prep_time, :total_time, :servings, presence: true
   validates :prep_time, :total_time, :servings, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
