d67ffb5e560f66f9ee7ce2e23f68906d90b282d3
Igor Dinic <123244689+dinicmajster@users.noreply.github.com>
2023-06-24 10:59:18 +0200

    Delete 20230621075600_add_columns_to_recipes.rb

--- db/migrate/20230621075600_add_columns_to_recipes.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToRecipes < ActiveRecord::Migration[7.0]
-  def change
-    add_column :recipes, :season, :string
-    add_column :recipes, :meal_type, :string
-    add_column :recipes, :dietary_requirements, :string
-  end
-end
